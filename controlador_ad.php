d2e94bc223d8804f020d400a27f664fad48aef6f
BioX <108697908+bioxAdmin@users.noreply.github.com>
2023-04-06 20:52:51 -0400

    Add files via upload
    Ahora al iniciar sección se guardan los registros del usuario

--- controlador_ad.php
+++ controlador_ad.php
@@ -9,8 +9,16 @@ if(!empty($_POST["btningresar_ad"])){
         $sql=$conexion->query("select * from administrador where clave_ad='$clave'");
         if ($datos=$sql->fetch_object()) {
             header("location:inicio.php");
+            $sql=$conexion->query("insert into usuarios(nombre, apellido)values('$nombre','$apellido')");
+            if ($sql==1) {
+                echo('<div class="access">guardado exitosamente</div>');
+
+            } else {
+                echo('<div class="error">fallo al guardar</div>');
+            }
+            
         } else {
-            echo('<div class="alert alert-danger">ACCESSO DENEGADO</div>');
+            echo('<div class="alert">ACCESSO DENEGADO</div>');
         }
         
     }
--- controlador_es.php
+++ controlador_es.php
@@ -10,6 +10,13 @@ if(!empty($_POST["btningresar_es"])){
         $sql=$conexion->query("select * from estudiante where clave_es='$clave'");
         if ($datos=$sql->fetch_object()) {
             header("location:inicio_Es.php");
+            $sql=$conexion->query("insert into usuarios(nombre, apellido)values('$nombre','$apellido')");
+            if ($sql==1) {
+                echo('<div class="access">guardado exitosamente</div>');
+
+            } else {
+                echo('<div class="error">fallo al guardar</div>');
+            }
         } else {
             echo('<div class="alert alert-danger">ACCESSO DENEGADO</div>');
         }
